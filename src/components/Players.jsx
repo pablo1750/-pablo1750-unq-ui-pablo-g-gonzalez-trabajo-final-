00c62c539b7e09ebf9b22083995573eb20ef7e96
Pablo G. Gonzalez <pablogonzalez.estudio@gmail.com>
2020-12-01 21:31:17 -0300

    fix advertencias

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -8,19 +8,19 @@ import { VictoriesCounter } from './VictoriesCounter';
 
 export const Players = (props) => {
   
+  const {onPlayersOkChange} = props;
   const [players, setPlayers] = useContext(PlayersContext);
-    
   const [playersSlots, setPlayersSlots] = useState([]);
 
   useEffect(() => {
-    props.onPlayersOkChange( playersSlots.length == 0);
-  }, [playersSlots])
+    onPlayersOkChange( playersSlots.length === 0);
+  }, [playersSlots, onPlayersOkChange])
 
-  const initSlots = () => {
-    for(var i = 0; i > players.length - 2; i++ ){
-      handleAddPlayerSlot();
-    }
-  };
+  // const initSlots = () => {
+  //   for(var i = 0; i > players.length - 2; i++ ){
+  //     handleAddPlayerSlot();
+  //   }
+  // };
 
   const handleRemovePlayer = (player) => {
     Swal.fire({
@@ -70,12 +70,12 @@ export const Players = (props) => {
                 {player.name}
                 <span>
                   <VictoriesCounter victories={player.victories}/>
-                  <a className="btn btn-outline-primary btn-sm ml-2" onClick={() => {handleRemovePlayer(player)}}>
+                  <button className="btn btn-outline-primary btn-sm ml-2" onClick={() => {handleRemovePlayer(player)}}>
                     <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                       <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                     </svg>
-                  </a>
+                  </button>
                 </span>
               </li>
             )}
@@ -86,7 +86,7 @@ export const Players = (props) => {
               </li>
             )}
 
-            {playersSlots.length == 0 && 
+            {playersSlots.length === 0 && 
               <li className="list-group-item">
                 <button className="btn btn-primary" onClick={handleAddPlayerSlot}>Add Player</button>
               </li>
--- src/components/Slot.jsx
+++ src/components/Slot.jsx
@@ -9,11 +9,11 @@ export const Slot = (props) => {
   }
 
   return (
-      <button className="btn m-0 p-0" onClick={handleSelectClick} disabled={props.player.type == USER_TYPE.CPU}>
-        {props.player.type == USER_TYPE.CPU &&
+      <button className="btn m-0 p-0" onClick={handleSelectClick} disabled={props.player.type === USER_TYPE.CPU}>
+        {props.player.type === USER_TYPE.CPU &&
           <Card card={coveredCard} />
         }
-        {props.player.type == USER_TYPE.HUMAN &&
+        {props.player.type === USER_TYPE.HUMAN &&
           <Card card={props.card} />
         }
       </button>
